e65443969e6ae59c8cce0d37b9b9d4c98a7abfd1
EliZucker <EliZucker@users.noreply.github.com>
2020-11-30 18:32:45 -0500

    Further refactor project component names.

--- main/flight_vehicle_model_data.h
+++ main/flight_vehicle_model_data.h
@@ -3,5 +3,10 @@
 // don't have a file system. It was created using the command:
 // xxd -i flight_vehicle_model.tflite > flight_vehicle_model_data.cc
 
+#ifndef FLIGHT_VEHICLE_MODEL_DATA_H_
+#define FLIGHT_VEHICLE_MODEL_DATA_H_
+
 extern const unsigned char g_flight_vehicle_model_data[];
 extern const int g_flight_vehicle_model_data_len;
+
+#endif  // FLIGHT_VEHICLE_MODEL_DATA_H_
--- main/person_detect_model_data.h
+++
@@ -1,7 +0,0 @@
-#ifndef FLIGHT_VEHICLE_MODEL_DATA_H_
-#define FLIGHT_VEHICLE_MODEL_DATA_H_
-
-extern const unsigned char g_flight_vehicle_model_data[];
-extern const int g_flight_vehicle_model_data_len;
-
-#endif  // FLIGHT_VEHICLE_MODEL_DATA_H_
